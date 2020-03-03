2f5bf603682b41f9d19e1a3607ebd5680223f414
Aaron Contreras <aaronlcaq@gmail.com>
2020-03-03 18:30:31 -0500

    Replace String for StringBuilder object

--- src/main/java/RaindropConverter.java
+++ src/main/java/RaindropConverter.java
@@ -1,31 +1,21 @@
 class RaindropConverter {
 
     String convert(int number) {
-	// keep a string for the return value
-	String s = "";
-
-	/* check each of the three conditions
-	   independently, adding to the empty string if
-	   the conditions are true*/
 	
-	if (number % 3 == 0) {
-	    s = s + "Pling";
+		StringBuilder builder = new StringBuilder();
+		
+		if (number % 3 == 0) {
+			builder.append("Pling");
+		}
+		if (number % 5 == 0) {
+			builder.append("Plang");
+		}
+		if (number % 7 == 0) {
+			builder.append("Plong");
+		}
+		if (builder.length() == 0) {
+			return Integer.toString(number);
+		}
+		return builder.toString();
 	}
-	if (number % 5 == 0) {
-	    s = s + "Plang";
-	}
-	if (number % 7 == 0) {
-	    s = s + "Plong";
-	}
-	
-	if (s.equals("")) {
-	    /* if after evaluating the three conditions
-	       the string remains empty, there are no
-	       factors*/
-	    return Integer.toString(number);
-	} else {
-	    return s;
-	}
-    }
-
 }
